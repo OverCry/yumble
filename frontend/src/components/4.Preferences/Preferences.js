010f9dff125693f71175ea182aac0b8cbc69dd25
OverCry <wcho400@aucklanduni.ac.nz>
2021-03-19 15:15:58 +1300

    Editted help messege

--- frontend/src/components/4.Preferences/Preferences.js
+++ frontend/src/components/4.Preferences/Preferences.js
@@ -134,12 +134,7 @@ function Preferences() {
           </button>
           <Help trigger={ButtonPopup} setTrigger={setButtonPopup}>
             <p>
-              If you are visiting this page, you are likely here because you are
-              searching for a random sentence. Sometimes a random word just is
-              not enough, and that is where the random sentence generator comes
-              into play. By inputting the desired number, you can make a list of
-              as many random sentences as you want or need. Producing random
-              sentences can be helpful in a number of different ways.
+              Enter your preferences for your session
             </p>
           </Help>
         </div>
